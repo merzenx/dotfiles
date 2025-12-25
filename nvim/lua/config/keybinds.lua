60ab54c7e97fb41e1befcb83f3cae54b7ee14065
mark-zen <nubdev.2001@gmail.com>
2025-12-26 00:26:05 +0700

    initial commit

---
+++ nvim/lua/config/keybinds.lua
@@ -0,0 +1,5 @@
+-- KEYBINDS
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>rl", "<cmd>source ~/.config/nvim/init.lua<cr>")
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,55 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+	dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+	branch = "master",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                        },
+                    },
+                },
+                highlight = {
+                    enable = true,
+                },
+                indent = { enable = true },
+                autotag = { enable = true },
+                ensure_installed = {
+                    "json",
+                    "python",
+                    "ron",
+                    "javascript",
+                    "haskell",
+                    "typescript",
+                    "tsx",
+                    "rust",
+                    "zig",
+                    "yaml",
+                    "html",
+                    "css",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "c",
+                    "dockerfile",
+                    "gitignore",
+                    "astro",
+                },
+                auto_install = false,
+            })
+        end
+    }
+}
