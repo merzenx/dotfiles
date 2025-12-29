eb0ca0909b8feeff8ec57fca53787177ee3a7efc
mark-zen <nubdev.2001@gmail.com>
2025-12-29 19:52:20 +0700

    chore(hypr): update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-function glog
-   journalctl -f -u gnirehtet-autorun.service -e 
-end
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
