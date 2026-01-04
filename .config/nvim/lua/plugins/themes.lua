c3605fdec3c5b93b371606204c20da9f57e05924
mark-zen <nubdev.2001@gmail.com>
2026-01-05 02:48:16 +0700

    chore: update hypr and nvim

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,3 +1,18 @@
 return {
-    'ellisonleao/gruvbox.nvim'
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            vim.cmd.colorschema = "tokyonight"
+            vim.api.nvim_set_hl(0,"Normal",{bg = "none"})
+        end
+    },  
+    {
+        "nvim-lualine/lualine.nvim",
+        dependendies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        opts = {
+            theme = "tokyonight"
+        }
+    }
 }
