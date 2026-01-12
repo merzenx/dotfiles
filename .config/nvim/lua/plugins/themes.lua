866ad432de75c4798194e9d5b36eeec7738c9354
mark-zen <nubdev.2001@gmail.com>
2026-01-12 19:27:28 +0700

    feat(nvim): remove comments and simplify theme config

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,16 +1,12 @@
 return {
     {
         "folke/tokyonight.nvim",
-        -- ใส่ priority และ lazy = false เพื่อให้โหลดเร็วและเสร็จก่อน plugin อื่นๆ
         lazy = false, 
         priority = 1000, 
         config = function()
-            -- 1. ตั้งค่า colorscheme หลัก
             vim.cmd.colorschema = "tokyonight"
             vim.api.nvim_set_hl(0,"Normal",{bg = "none"})
             
-            -- 2. *สำคัญ* เรียก Lualine ให้ตั้งค่าใหม่ (Re-setup) 
-            --    เพื่อให้มั่นใจว่าดึงค่า theme "tokyonight" ที่โหลดแล้วมาใช้
             local lualine = require("lualine")
             if lualine then
                 lualine.setup({ theme = "tokyonight" })
@@ -19,15 +15,9 @@ return {
     },  
     {
         "nvim-lualine/lualine.nvim",
-        -- ตรวจสอบให้แน่ใจว่า dependencies ถูกต้อง
         dependencies = {
             "nvim-tree/nvim-web-devicons",
-            "folke/tokyonight.nvim", -- เพิ่มอันนี้ (ถ้ายังไม่ได้เพิ่ม)
+            "folke/tokyonight.nvim",
         },
-        -- *ลบ* การตั้งค่า theme ออกจาก opts 
-        -- เพราะเราจะไปกำหนดใน config ของ tokyonight แทน (ในขั้นตอนที่ 2 ด้านบน)
-        -- opts = {
-        --     theme = "tokyonight" 
-        -- }
     }
 }
