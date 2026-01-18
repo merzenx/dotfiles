b1bfa1570fdad569fb56a3cae0f277d9cd960593
mark-zen <nubdev.2001@gmail.com>
2026-01-18 23:54:28 +0700

    feat(config): remove fish hyprland kitty lazygit configs

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# opencode
-fish_add_path /home/mark/.opencode/bin
