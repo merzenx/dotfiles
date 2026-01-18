66972ddae154030542ba93d7b638c40a9be95e0a
mark-zen <nubdev.2001@gmail.com>
2026-01-18 23:52:19 +0700

    feat(dotfiles): add fish shell and hyprland configuration

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# opencode
+fish_add_path /home/mark/.opencode/bin
