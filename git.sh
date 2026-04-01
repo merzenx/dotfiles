3029f074e785467000d64cd4b8d076150565772b
mark-zen <nubdev.2001@gmail.com>
2026-04-01 23:19:14 +0700

    update git

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git config --global user.name "mark-zen"
+git config --global user.name "merzenx"
 git config --global user.email "nubdev.2001@gmail.com"
 git config --global pull.rebase true
 git config --global push.autoSetupRemote true
